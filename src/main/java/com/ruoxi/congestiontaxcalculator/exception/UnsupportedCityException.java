63f96c1a7e27b335be84b744cb8d06c3eb9412d9
ruoxizhang <ruoxizh@gmail.com>
2022-05-16 00:38:57 +0200

    congestion tax calculator, initial commit

---
+++ src/main/java/com/ruoxi/congestiontaxcalculator/exception/UnsupportedCityException.java
@@ -0,0 +1,8 @@
+package com.ruoxi.congestiontaxcalculator.exception;
+
+public class UnsupportedCityException extends RuntimeException {
+
+  public UnsupportedCityException(String message) {
+    super(message);
+  }
+}
